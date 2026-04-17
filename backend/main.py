aaa88f791eb36bf4362adb8c5c8e3110fbfc73ff
Nicolas Bury <nicolas.bury@outlook.com>
2026-04-17 16:37:33 +0200

    Implement GET /transactions API endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,31 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from sqlmodel import Session, select
+
+from database.database import engine
+from database.models import Transaction
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    """
+    Health check endpoint.
+    Confirms the API is running.
+    """
+    return {"message": "Finance Analytics API is running"}
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/transactions")
+def get_transactions():
+    """
+    Retrieve all transactions from the database.
+
+    Returns:
+        List of transaction records in JSON format.
+    """
+
+    with Session(engine) as session:
+
+        statement = select(Transaction)
+        transactions = session.exec(statement).all()
+        return transactions
