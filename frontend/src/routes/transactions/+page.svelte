54c2c5948f600ff6efbf93991d898da355eb5689
Nicolas Bury <nicolas.bury@outlook.com>
2026-04-19 17:13:34 +0200

    Refactor transaction loading logic and improve loading state handling

--- frontend/src/routes/transactions/+page.svelte
+++ frontend/src/routes/transactions/+page.svelte
@@ -3,14 +3,15 @@
     import { onMount } from "svelte";
 
     let transactions = $state([]);
+    let loading = $state(true);
 
     async function loadTransactions() {
         const response = await fetch(
             "http://127.0.0.1:8000/transactions"
         );
         transactions = await response.json();
+        loading = false;
     }
-
     onMount(loadTransactions);
 
 </script>
@@ -29,18 +30,24 @@
     </thead>
 
     <tbody>
-
-        {#each transactions as transaction}
-
-        <tr>
-            <td>{transaction.id}</td>
-            <td>{transaction.description}</td>
-            <td>{transaction.amount}</td>
-            <td>{transaction.date}</td>
-            <td>{transaction.category}</td>
-        </tr>
-
-    {/each}
-
+        {#if loading}
+            <tr>
+                <td colspan="5">Loading data...</td>
+            </tr>
+        {:else if transactions.length === 0}
+            <tr>
+                <td colspan="5">No transactions found</td>
+            </tr>
+        {:else}
+            {#each transactions as transaction}
+                <tr>
+                    <td>{transaction.id}</td>
+                    <td>{transaction.description}</td>
+                    <td>{transaction.amount}</td>
+                    <td>{transaction.date}</td>
+                    <td>{transaction.category}</td>
+                </tr>
+            {/each}
+        {/if}
     </tbody>
 </table>
