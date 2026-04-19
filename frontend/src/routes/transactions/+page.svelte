52de6bf40c97ef00465570ad3b2cc946473f4d08
Nicolas Bury <nicolas.bury@outlook.com>
2026-04-19 16:44:26 +0200

    Initial transaction page table

--- frontend/src/routes/transactions/+page.svelte
+++ frontend/src/routes/transactions/+page.svelte
@@ -1,4 +1,18 @@
 <script>
+
+    import { onMount } from "svelte";
+
+    let transactions = $state([]);
+
+    async function loadTransactions() {
+        const response = await fetch(
+            "http://127.0.0.1:8000/transactions"
+        );
+        transactions = await response.json();
+    }
+
+    onMount(loadTransactions);
+
 </script>
 
 <h1>Transactions</h1>
@@ -15,5 +29,18 @@
     </thead>
 
     <tbody>
+
+        {#each transactions as transaction}
+
+        <tr>
+            <td>{transaction.id}</td>
+            <td>{transaction.description}</td>
+            <td>{transaction.amount}</td>
+            <td>{transaction.date}</td>
+            <td>{transaction.category}</td>
+        </tr>
+
+    {/each}
+
     </tbody>
 </table>
